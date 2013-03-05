2e1d512c2105e768b3fc2e9e7d20787fabe41e8d
Alexander Tarnowski <alexander@local>
2013-03-06 00:06:45 +0100

    Just added a class comment

--- tests/src/test/java/se/tarnowski/agdp2013/cucumber/RunInvoiceTest.java
+++ tests/src/test/java/se/tarnowski/agdp2013/cucumber/RunInvoiceTest.java
@@ -3,6 +3,7 @@ package se.tarnowski.agdp2013.cucumber;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
+// This file isn't required if the feature is executed in directly in IDEA
 @RunWith(Cucumber.class)
 @Cucumber.Options(format={"pretty", "html:build/cucumber"})
 public class RunInvoiceTest {
